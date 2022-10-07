477e6e9f1bc03c48ee38a4dcb46fc412ea2ab63a
HatchiTheHorse <113034099+HatchiTheHorse@users.noreply.github.com>
2022-10-07 07:30:05 -0500

    Initial Commit

---
+++ AppChallengeMeds/AppChallengeMedsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppChallengeMedsApp.swift
+//  AppChallengeMeds
+//
+//  Created by AM Student on 10/7/22.
+//
+
+import SwiftUI
+
+@main
+struct AppChallengeMedsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
